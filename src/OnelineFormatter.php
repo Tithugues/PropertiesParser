cb80b3425b96a63d1429461b4aa0e6a4c76aed5c
Hugues Peccatte <hugues.peccatte@gmail.com>
2018-12-03 00:03:57 +0100

    Add comment to explain how to find the line break to replace

--- src/OnelineFormatter.php
+++ src/OnelineFormatter.php
@@ -15,7 +15,9 @@ class OnelineFormatter implements Formatter
         \preg_match_all('`(?<!\\\\)(?:\\\\\\\\)*\\\\\R`', $content, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE);
         $matches = \array_reverse($matches);
         foreach ($matches as $match) {
-            $positionLineBreak = strpos($content, "\n", $match[0][1]-1);
+            //Because $match[0][0] can be longer than 2 characters (when escaped backslashes before the escaped line
+            // break), we need to find the line break character position.
+            $positionLineBreak = $match[0][1] + strlen($match[0][0]) - 1;
             $content = \substr($content, 0, $positionLineBreak-1) . ltrim(substr($content, $positionLineBreak + 1));
         }
         return $content;
